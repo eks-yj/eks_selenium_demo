6af2f0abb9c6338b363ee1d30df940ad5c25462a
eks-yj <2794336931@qq.com>
2019-07-25 21:12:10 +0800

    20190725_2111_Support mouse

---
+++ src/main/java/com/eks/utils/TouTiaoUtils.java
@@ -0,0 +1,67 @@
+package com.eks.utils;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
+
+public class TouTiaoUtils {
+    private static String cookieSetPathString = null;
+    static {
+        String filePathString = FileUtils.generatePathBaseProjectPath("extra/driver/chromedriver_win32/chromedriver.exe");
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,filePathString);
+        cookieSetPathString = FileUtils.generatePathBaseProjectPath("extra/object/cookieSet");
+    }
+    public static WebDriver login(List<String> argumentStringList){
+        String[] argumentStringArray = argumentStringList.toArray(new String[argumentStringList.size()]);
+        return login(argumentStringArray);
+    }
+    public static WebDriver login(String... argumentStringArray){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(argumentStringArray);
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+        WebDriver.Options options = webDriver.manage();
+        WebDriver.Window window = options.window();
+        window.maximize();
+        webDriver.get("https://www.toutiao.com/");
+
+        Set<Cookie> cookieSet = SerializeUtils.readObject(cookieSetPathString);
+        if (cookieSet != null && cookieSet.size() > 0){
+            for(Cookie cookie : cookieSet){
+                options.addCookie(cookie);
+            }
+        }
+        WebDriver.Navigation navigation = webDriver.navigate();
+        navigation.refresh();
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver,60);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".name")));
+
+        cookieSet = options.getCookies();
+        if (cookieSet != null){
+            SerializeUtils.writeObject(cookieSetPathString,cookieSet);
+        }
+        return webDriver;
+    }
+    public static void sendKeys(WebDriver webDriver,String cssSelectorString,String contentString){
+        WebElement webElement = webDriver.findElement(By.cssSelector(cssSelectorString));
+        webElement.sendKeys(contentString);
+    }
+    public static void click(WebDriver webDriver,int xOffset, int yOffset){
+        Actions actions = new Actions(webDriver);
+        WebElement webElement = webDriver.findElement(By.cssSelector("body"));
+        actions.moveToElement(webElement,0,0);
+        actions.moveByOffset(xOffset,yOffset).click();
+        actions.release();
+        actions.perform();
+    }
+    public static Object executeScript(WebDriver webDriver,String scriptString){
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor)webDriver;
+        return javascriptExecutor.executeScript(scriptString);
+    }
+}
---
+++ src/test/java/com/eks/utils/TouTiaoUtilsTest.java
@@ -0,0 +1,43 @@
+package com.eks.utils;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TouTiaoUtilsTest {
+    @Test
+    public void test1() throws InterruptedException {
+        List<String> argumentStringList = new ArrayList<>();
+        //设置为 headless 模式
+//        argumentStringList.add("--headless");
+        //以中文utf-8编码格式启动浏览器
+        argumentStringList.add("--lang=zh_CN.UTF-8");
+        //设置浏览器最大化,其实此处无必要，直接在代码中设置最大化即可，不建议使用
+        argumentStringList.add("--start-maximized");
+        WebDriver webDriver = TouTiaoUtils.login(argumentStringList);
+        Thread.sleep(3000);
+        TouTiaoUtils.sendKeys(webDriver, ".title-input","Hello");
+        Thread.sleep(3000);
+        WebElement webElement = webDriver.findElement(By.cssSelector(".show-image-uploader.show-uploader"));
+        webElement.click();
+        Thread.sleep(3000);
+        String filePathString = FileUtils.generatePathBaseProjectPath("extra/image/gratisography/gratisography-157H.jpg");
+        TouTiaoUtils.sendKeys(webDriver, "#fileElem",filePathString);
+        Thread.sleep(3000);
+        WebDriver.Options options = webDriver.manage();
+        WebDriver.Window window = options.window();
+        Dimension dimension = window.getSize();
+        int heightInt = dimension.getHeight();
+        while (true){
+            Thread.sleep(3000);
+            TouTiaoUtils.click(webDriver,1,heightInt / 2);
+            TouTiaoUtils.executeScript(webDriver,"console.log('hello world')");
+        }
+    }
+}
