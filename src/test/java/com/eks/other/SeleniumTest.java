47b320944ed9f3e7c442a9d59730d7978e772b39
eks <2794336931@qq.com>
2020-07-17 16:32:46 +0800

    enhance "SeleniumTest".

--- src/test/java/com/eks/other/SeleniumTest.java
+++ src/test/java/com/eks/other/SeleniumTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SeleniumTest {
@@ -18,12 +17,7 @@ public class SeleniumTest {
         String pathBaseProjectPath = FileUtils.getPathBaseProjectPath("extra/driver/chromedriver_win32/84.0.4147.30/chromedriver.exe");
         SeleniumUtils.setChromeDriverProperty(new File(pathBaseProjectPath));
         WebDriver webDriver = SeleniumUtils.getChromeDriver(true, false);
-        webDriver.manage().window().maximize();
-        //与浏览器同步非常重要，必须等待浏览器加载完毕
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //下面这句等价于webDriver.navigate().to("www.baidu.com");
-        webDriver.navigate().to("https://www.lz13.cn/zheli/184189.html");
-        //在输入框中填写要搜索的内容
+        webDriver.get("https://www.lz13.cn/zheli/184189.html");
         WebElement webElement = webDriver.findElement(By.cssSelector("#node-8890 > div.PostContent"));
         log.info("webElement:{}", webElement.getAttribute("outerHTML"));
     }
